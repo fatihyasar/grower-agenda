2b3b58d0a7af34d055177f0a2552f71b8a064f9d
Fatih YASAR <fyasar@luteg.com>
2018-10-17 16:37:17 +0300

    .

--- index.js
+++ index.js
@@ -6,24 +6,29 @@ client.on('connect', function () {
     console.log('Connected MQTT server');
 });
 
-//Start lights - that works every day at 16:30 
+//Start lights - that works every day at 08:01 
+console.log('scheduling lights management on crontab');
+console.log('creating turn on task');
 var task1 = schedule.scheduleJob('01 08 * * *', function(fireDate) {
+    console.log('Lights turn on task started with fireDate : ' + fireDate);
     var alert = "Lights is turned ON";
     sendNotificaion(alert);
     console.log(alert);            
 });
 
 
-//Stop lights - that works every day at 16:33 
-var task2 = schedule.scheduleJob('01 20 * * *', function(fireDate) {
+//Stop lights - that works every day at 19:01 
+console.log('creating turn off task');
+var task2 = schedule.scheduleJob('01 19 * * *', function(fireDate) {
+    console.log('Lights turn on task started with fireDate : ' + fireDate);
     var alert = "Lights is turned OFF";
     console.log(alert);            
     sendNotificaion(alert);
 });  
 
 
-
 function sendNotificaion(alert) {
+    console.log('sending alert notification : ' + alert);
     var topic = "/brokers/pushnotification";
     var data = {};
     data.payload = "";
