1e9f3663adf275b3483e6ff62f5408d6e11a2777
Fatih YASAR <fyasar@luteg.com>
2018-12-11 10:27:10 +0300

    Airfun

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ client.on('connect', function () {
 
 //Start lights - that works every day at 08:01 
 console.log('scheduling lights management on crontab');
-console.log('creating turn on task');
-var task1 = schedule.scheduleJob('01 08 * * *', function(fireDate) {
+console.log('creating light turn on task');
+var task1 = schedule.scheduleJob('00 08 * * *', function(fireDate) {
     console.log('Lights turn on task started with fireDate : ' + fireDate);
     var alert = "Lights is turned ON";
     sendNotificaion(alert);
@@ -18,13 +18,26 @@ var task1 = schedule.scheduleJob('01 08 * * *', function(fireDate) {
 
 
 //Stop lights - that works every day at 19:01 
-console.log('creating turn off task');
-var task2 = schedule.scheduleJob('01 19 * * *', function(fireDate) {
+console.log('creating light turn off task');
+var task2 = schedule.scheduleJob('00 19 * * *', function(fireDate) {
     console.log('Lights turn on task started with fireDate : ' + fireDate);
     var alert = "Lights is turned OFF";
     console.log(alert);            
     sendNotificaion(alert);
 });  
+
+//Start airfun - that works every day at 08:01 
+console.log('scheduling airfun management on crontab');
+console.log('creating airfun - turn on task');
+var task1 = schedule.scheduleJob('0 * * * *', function(fireDate) {
+    console.log('Airfun turn on task started with fireDate : ' + fireDate);
+    var alert = "Airfun is turned ON";
+    sendNotificaion(alert);    
+    console.log(alert);    
+    
+    var topic = "/actuators/plugs/command/2/on";
+    client.publish(topic, JSON.stringify({}));        
+});
 
 
 function sendNotificaion(alert) {
@@ -33,7 +46,11 @@ function sendNotificaion(alert) {
     var data = {};
     data.payload = "";
     data.alert = alert;
-    client.publish(topic, JSON.stringify(data));        
+    client.publish(topic, JSON.stringify(data));     
+    
+    var topic = "/actuators/plugs/command/2/off";
+    client.publish(topic, JSON.stringify({}));        
+    
 }
 
 
